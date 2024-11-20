6971050f560539c6576490481f644596cc737e4b
ThomasDlx <thom.delcroix@gmail.com>
2024-11-20 15:19:55 +0100

    first commit

---
+++ src/Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Leonardo;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+var results = new List<Task<FibonacciResult>>();
+foreach (var input in args)
+{
+    var int32 = Convert.ToInt32(input);
+    var r = Task.Run(() =>
+    {
+        var result = Fibonacci.Run(int32);
+        return new FibonacciResult(int32, result);
+    });
+    results.Add(r);
+}
+
+Task.WaitAll(results.ToArray());
+stopwatch.Stop();
+Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+foreach (var result in results)
+{
+    Console.WriteLine($"Fibonacci of {result.Result.Input} is {result.Result}");
+}
---
+++ src/Leonardo.Web/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/Fibonacci", 
+    async () => await Leonardo.Fibonacci.RunAsync(new []{"44", "43"}));
+
+app.Run();
